9a368bbc92b66639c43b8b6c37624a27cf983cd5
nine <132211467+nattakon4818@users.noreply.github.com>
2025-07-22 16:39:10 +0700

    Update test.py

--- funny/python/งานอ.สมชัย/บทที่ 3/test.py
+++ funny/python/งานอ.สมชัย/บทที่ 3/test.py
@@ -88,3 +88,44 @@ elif player1 == 3 and player2 == 3:
     print("drew")
 else:
     print("Error")
+
+
+
+
+
+print("Program Find Maximum Dight")
+while True:
+    number = input("Enter integer number(0-exit) : ")
+
+    if number == "0":
+        print("exit program")
+        break
+
+    max_digit = max(number)
+    print("Maximum Digit of integer number", number, " : ", max_digit)
+
+
+
+
+
+
+print("Program Find Maximum Vulue")
+num = int(input("Enter number of value(>=) : "))
+
+if num < 1:
+    print("Value input not correct.""\nExit program")
+else:
+    numbers = []
+
+    print(f"\nProgram get value {num} numbers.")
+
+    for i in range(1, num + 1):
+        number = int(input(f"Enter value Number #{i} : "))
+        numbers.append(number)
+
+    print("Your enter number : ", ", ".join(str(n) for n in numbers))
+        
+    max_value = max(numbers)
+
+    print("Maximum value number is", max_value)
+    print("Exit Program")
