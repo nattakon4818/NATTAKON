d02ba858cbaff61fc617034e1236f6279b60ad49
NATTAKON <nattakon4818@gmail.com>
2025-07-25 18:39:53 +0700

    c

---
+++ work/work-3/python.py
@@ -0,0 +1,18 @@
+import time
+
+time.sleep(5)
+
+song = [
+    ["", ],
+    ["", ],
+    ["", ],
+    ["", ],
+    ["", ],
+    ["", ],
+    ["", ],
+    ["", ],
+]
+
+for line in song:
+    print(line[0] + "\n")
+    time.sleep(line[1])
