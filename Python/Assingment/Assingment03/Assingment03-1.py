5fddb1107bfdf22dd52c526c26e3c3f820ed5adc
NATTAKON <nattakon4818@gmail.com>
2025-08-11 19:01:54 +0700

    python

---
+++ Python/Assingment/Assingment03/Assingment03-1.py
@@ -0,0 +1,8 @@
+# while True:
+#     print(f"{"=" * 17}",f"{"| Tax Main Menu |"}")
+
+
+# print(f"{"=" * 49}",f"{"|\t\tCurrency Convert\t\t|"}",f"{"=" * 49}",f"|{" 0. Exit":<47}|",f"|{" 1. Enter money":<47}|",f"|{" 2. Convert THB to USD(33 THB / 1 USD)":<47}|",f"|{" 3. Convert THB to USD(0.2 THB / 1 JPY)":<47}|",f"|{" 4. Convert THB to USD(37 THB / 1 EUR)":<47}|",f"{"=" * 49}", sep='\n')
+print(f"{"=" * 17}",f"{"| Tax Main Menu |"}",f"{"=" * 17}",f"{" 0. Exit"}",f"{" 1. Input Salaey(0.00)"}",f"{" 2. Display Tax"}", sep='\n')
+
+choice = int(input("Enter choice : "))
