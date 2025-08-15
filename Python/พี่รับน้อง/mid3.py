ebd839a58f6a2ca9472d62757d2fd95a88640673
NATTAKON <nattakon4818@gmail.com>
2025-08-15 23:00:45 +0700

    html css

---
+++ Python/พี่รับน้อง/mid3.py
@@ -0,0 +1,8 @@
+h = "| Calsius  | Farenheit |  Kelvin |"
+l = "=" * len(h)
+
+start = int(input("Enter start Calsius: "))
+end = int(input("Enter end Calsius: "))
+step = int(input("Enter start Calsius: "))
+
+print(l, h, l, sep='\n')
