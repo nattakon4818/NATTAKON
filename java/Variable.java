1733cd45b70c8291274f4d69cf0eb218a30555cd
NATTAKON <nattakon4818@gmail.com>
2025-11-16 17:11:03 +0700

    java

---
+++ java/Variable.java
@@ -0,0 +1,56 @@
+public class Variable {
+    public static void main(String[] args) {
+        /// นิยามค่าตัวแปรแบบไม่ระบุค่าเริ่มต้น
+
+        // int number1;
+        // boolean status;
+        // char alphabet;
+        // float number2;
+        // double number3;
+
+        ///////////////////////////////////
+        /// นิยามค่าตัวแปรแบบระบุค่าเริ่มต้น
+
+        // int number1 = 10;
+        // float number2 = 20.5f;
+        // char alphabet = 'A';
+        // boolean status = true;
+
+        ///////////////////////////////////
+        /// หลายบรรทัดในครั้งเดียว
+
+        // int a = 0, b = 1, c = 2;
+
+        ///////////////////////////////////
+        /// ค่าที่สามารถเปลี่ยนแปลงได้
+
+        // int num1 = 10;
+        int num2 = 20;
+        // num1 = 100;
+        // System.out.println("The numerical value of 1 is " + num1);
+        // System.out.println("The numerical value of 2 is " + num2);
+        System.out.println("ค่าของ num2 คือ " + num2);
+
+        ///////////////////////////////////
+        /// ค่าคงที่
+
+        // final int NUM1 = 10;
+        // final int NUM2 = 20;
+        // System.out.println("The numerical value of 1 is " + NUM1);
+        // System.out.println("The numerical value of 2 is " + NUM2);
+
+        ///////////////////////////////////
+        /// Global Variable
+        
+        // int a = 10;
+        // System.out.println(a);
+
+        /// Local Variable 
+        // {
+        //     int c = 20;
+        //     System.out.println(c);
+        // }
+
+        //////////////////////////////////
+    }
+}
