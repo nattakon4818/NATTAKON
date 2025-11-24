6bf2bd1ffec79f475090a5be0b3b957730633002
NATTAKON <nattakon4818@gmail.com>
2025-11-24 18:27:12 +0700

    java

---
+++ java/Assign/Assign7.java
@@ -0,0 +1,30 @@
+// โปรแกรมแปลงหน่วยอุณหภูมิ
+
+import java.util.Scanner;
+
+public class Assign7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // แปลงจาก fahrenheit เป็น celsius
+
+        // System.out.print("Enter temperature (Fahrenheit) : ");
+        // float fahrenheit = scanner.nextFloat();
+        // float celsius = (fahrenheit - 32) * 5 / 9;
+        // System.out.println(fahrenheit + " Fahrenheit is " + celsius + " Celsius");
+
+        // แปลงจาก celsius เป็น fahrenheit
+
+        // System.out.print("Enter temperature (Celsius) : ");
+        // float celsius = scanner.nextFloat();
+        // float fahrenheit = (celsius * 9 / 5) + 32;
+        // System.out.println(celsius + " Celsius " + fahrenheit + " Fahrenheit");
+    }
+}
+/*
+ * สูตรแปลงหน่วยอุณหภูมิ
+ * celsius to fahrenheit
+ * celsius = (fahrenheit - 32) * 5/9
+ * 
+ * fahrenheit to celsius
+ * fahrenheit = (celsius * 9/5) + 32
+ */
